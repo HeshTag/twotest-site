a7c8e970e2cad695d6f2358ae5837a510415be60
Just Name <face.geek@bk.ru>
2021-12-02 15:23:05 +0300

    Удаление папки goshadudar

--- gulp-js/gulpfile.js
+++ gulp-js/gulpfile.js
@@ -51,11 +51,3 @@ function watchFiles() {
 gulp.task('default', gulp.parallel(server, gulp.series(styleScss, watchFiles)));
 
 // exports.def = defaultSomeTask;
-
-// Bootstrap 5
-// https://www.youtube.com/watch?v=kaLZgD-Ctss&list=PLD-piGJ3Dtl3qZFKzosT47lCCMEcqMK_L&index=1
-// https://www.youtube.com/watch?v=z6a2BGeYwpk&list=PLD-piGJ3Dtl3qZFKzosT47lCCMEcqMK_L&index=2 
-// https://www.youtube.com/watch?v=5Q-lv5xuFu0&list=PLD-piGJ3Dtl3qZFKzosT47lCCMEcqMK_L&index=3 
-// https://www.youtube.com/watch?v=cyyy1nisy1I&list=PLD-piGJ3Dtl3qZFKzosT47lCCMEcqMK_L&index=4 
-// https://www.youtube.com/watch?v=EP4qGTSEs4U&list=PLD-piGJ3Dtl3qZFKzosT47lCCMEcqMK_L&index=5 
-// https://www.youtube.com/watch?v=A6r6c5aX4XE&list=PLD-piGJ3Dtl3qZFKzosT47lCCMEcqMK_L&index=6
